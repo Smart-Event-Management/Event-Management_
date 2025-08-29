ae29ba784e263fc55950f436a9dc99374be3fcb2
chkushal410 <chkushal410@gmail.com>
2025-08-29 20:52:18 +0530

    Event_registration table

---
+++ public/event_registration.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 24, 2025 at 08:50 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `alumnidb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_registration`
+--
+
+CREATE TABLE `event_registration` (
+  `id` int(11) NOT NULL,
+  `Event_name` varchar(20) NOT NULL,
+  `Time` int(20) NOT NULL,
+  `Date` varchar(20) NOT NULL,
+  `Venue` varchar(60) NOT NULL,
+  `Department` varchar(40) NOT NULL,
+  `Poster_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `event_registration`
+--
+
+INSERT INTO `event_registration` (`id`, `Event_name`, `Time`, `Date`, `Venue`, `Department`, `Poster_name`) VALUES
+(0, '', 0, '', '', '', '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
