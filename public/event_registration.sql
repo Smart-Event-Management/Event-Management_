9bccbea4b30f618c4f5797f6e108fedfae090914
chkushal410 <chkushal410@gmail.com>
2025-09-05 13:38:57 +0530

    updated

--- public/event_registration.sql
+++ public/event_registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 24, 2025 at 08:50 AM
+-- Generation Time: Sep 05, 2025 at 10:08 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,19 +30,40 @@ SET time_zone = "+00:00";
 CREATE TABLE `event_registration` (
   `id` int(11) NOT NULL,
   `Event_name` varchar(20) NOT NULL,
-  `Time` int(20) NOT NULL,
+  `Time` varchar(20) NOT NULL,
   `Date` varchar(20) NOT NULL,
   `Venue` varchar(60) NOT NULL,
   `Department` varchar(40) NOT NULL,
-  `Poster_name` varchar(30) NOT NULL
+  `Poster_name` varchar(30) NOT NULL,
+  `Event_links` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `event_registration`
 --
 
-INSERT INTO `event_registration` (`id`, `Event_name`, `Time`, `Date`, `Venue`, `Department`, `Poster_name`) VALUES
-(0, '', 0, '', '', '', '');
+INSERT INTO `event_registration` (`id`, `Event_name`, `Time`, `Date`, `Venue`, `Department`, `Poster_name`, `Event_links`) VALUES
+(1, 'Club_spark', '2:00pm', '06/07/2025', 'HT3 lab', 'IT', 'Club_spark', 'https://Club_spark_google_form');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `event_registration`
+--
+ALTER TABLE `event_registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `event_registration`
+--
+ALTER TABLE `event_registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
