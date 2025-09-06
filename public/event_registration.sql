6dd824741e453e594ae76a6fb8df0e08687d98bc
chkushal410 <chkushal410@gmail.com>
2025-09-06 14:53:20 +0530

    updated

--- public/event_registration.sql
+++ public/event_registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2025 at 12:48 PM
+-- Generation Time: Sep 06, 2025 at 11:15 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -45,7 +45,13 @@ CREATE TABLE `event_registration` (
 INSERT INTO `event_registration` (`id`, `Event_name`, `Time`, `Date`, `Venue`, `Department`, `Poster_name`, `Event_links`) VALUES
 ('e001', 'Club_spark', '2:00pm', '06/07/2025', 'HT3 lab', 'IT', 'Club_spark.jpg', 'https://Club_spark_google_form'),
 ('e002', 'Aiml Arena\'25 coding', '', '03.09.2025', 'HT Labs1&2', '', 'Aiml_Arena\'25_coding_contest.j', ''),
-('e003', 'Amaravati quantunm valley hackathon 2025', '11:00am', '28.08.2025', '', 'computer science & business system', 'Amaravati_quantunm_valley_hack', '');
+('e003', 'Amaravati quantunm valley hackathon 2025', '11:00am', '28.08.2025', '', 'computer science & business system', 'Amaravati_quantunm_valley_hack', ''),
+('e004', 'Colorido', '', '15-16 February', '', '', 'Colorido.jpg', ''),
+('e005', 'Engineers\'day celebration Quiz', '10:00AM', '26.09.2025', '', '', 'Engineer\'sday_celebration.jpg', ''),
+('e006', 'International yoga day', '11:00 AM', '21.06.2025', '', '', '', ''),
+('e007', 'Medhanvesh', '', '06.07.2025', '', '', 'Medhanvesh.jpg', ''),
+('e008', 'Photography talk', '11:00 AM', '23.04.2025', '', '', 'Photography_club.jpg', ''),
+('e009', 'Renewaloegy elergy (Poster Persontantion)', '2:00 PM', '30.06.2025', '', '', 'Renewaloegy_elergy.jpg', '');
 
 --
 -- Indexes for dumped tables
